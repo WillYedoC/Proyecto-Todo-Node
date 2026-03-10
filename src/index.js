33912b7ac5a80e5a980d1eb97cb3c5a560ba8b8d
WillYedoC <169630283+WillYedoC@users.noreply.github.com>
2026-03-10 09:22:59 -0400

    PTN-4: Correccion en el index para rutas no encontradas(404)

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const categoryRoutes = require('./routes/category.routes');
 app.use('/users', userRoutes);
 app.use('/categories', categoryRoutes);
 
-app.get('*', (req, res) => {
-  res.json({status : '404', message: 'Ruta no encontrada'});
+app.get((req, res) => {
+  res.status(404).json({ error: 'Ruta no encontrada' });
 });
 
 const PORT = process.env.PORT || 3000;
