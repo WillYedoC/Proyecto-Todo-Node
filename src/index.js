5f300bff99931449d497a370daa2370859cb1c2c
WillYedoC <169630283+WillYedoC@users.noreply.github.com>
2026-03-09 22:47:18 -0400

    PTN-3: Implementacion de los controladores y las rutas para los usuarios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const app = express();
 require('dotenv').config();
+require('./db/connection');
 
+const app = express();
 app.use(express.json());
+
+// Rutas
+const userRoutes = require('./routes/user.routes');
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
