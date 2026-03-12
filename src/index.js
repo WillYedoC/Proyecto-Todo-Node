c2d32fc0441b4d1e76c8fdfaff698ba672274072
WillYedoC <169630283+WillYedoC@users.noreply.github.com>
2026-03-12 00:45:49 -0400

    PTN-4: Correcciones de los controladores y el decorator

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.json());
+// Rutas
+const categoryRoutes = require('./routes/category.routes');
+app.use('/api/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
